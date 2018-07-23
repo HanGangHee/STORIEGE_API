d29e4a8f37e2577c0e0e2f2e57672860e80abbe2
HanGangHee <tkdlaos21@ajou.ac.kr>
2018-07-23 14:25:31 +0900

    auth 테트스

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -19,19 +19,21 @@ suite('GET /', () => {
     })
 })
 
-suite('GET /user/login', () => {
+suite('GET /user/auth', () => {
     test('should respond with text message user "gksrkdgml"', (done) => {
         request(app.listen())
-            .get('/user/login')
+            .post('/user/auth')
+            .send({ userID: "test", pwd: "1234"})
             .expect(200)
             .end((err, res) => {
                 if(err){
                     done(err)
                     return
                 }
+                console.log(res.text)
                 let result = JSON.parse(res.text)
-                expect(result['userID']).to.equal('gksrkdmgl')
-                expect(result['pwd']).to.equal('qewr')
+                expect(result['userID']).to.equal('test')
+                expect(result['pwd']).to.equal('1234')
                 expect(result['name']).to.equal('한강희')
                 expect(result['sex']).to.equal('남')
                 done()
