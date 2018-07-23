76224c4d8cc7a946317e6c7446d442c963861e9d
HanGangHee <tkdlaos21@ajou.ac.kr>
2018-07-23 14:22:57 +0900

    jwt-secret 설정

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
+
 import express from 'express';
 import bodyParser from 'body-parser'
+import morgan from 'morgan'
+
+
 import index from './router/index'
+import {JWT} from './credential'
 
 const app = express()
 
 module.exports = app
 
-
 app.use(express.static('public'))
+// parse JSON and URL_encoded query
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
+
+//print log on console
+app.use(morgan('dev'))
+// route 설정
 app.use(index)
+// JWT KEY SET
+app.set('jwt-secret', JWT.secret)
+
 
 
 const port = process.env.port || 3000
