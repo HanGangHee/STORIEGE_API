963f70677b9042cb8cbcbb3aa570dcb80d41be12
HanGangHee <tkdlaos21@ajou.ac.kr>
2018-07-24 17:03:40 +0900

    JWT를 통한 User 정보 받기. 테스트 추가

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -19,10 +19,10 @@ suite('GET /', () => {
     })
 })
 
-suite('GET /user/auth', () => {
-    test('should respond with text message user "gksrkdgml"', (done) => {
+suite('POST /user/login', () => {
+    test('should respond with text message  "logged in successfully"', (done) => {
         request(app.listen())
-            .post('/user/auth')
+            .post('/user/login')
             .send({ userID: "test", pwd: "1234"})
             .expect(200)
             .end((err, res) => {
@@ -30,14 +30,72 @@ suite('GET /user/auth', () => {
                     done(err)
                     return
                 }
-                console.log(res.text)
-                let result = JSON.parse(res.text)
-                expect(result['userID']).to.equal('test')
-                expect(result['pwd']).to.equal('1234')
-                expect(result['name']).to.equal('한강희')
-                expect(result['sex']).to.equal('남')
+                let result = res.body
+                expect(result['message']).to.equal('logged in successfully')
+                // expect(result['pwd']).to.equal('1234')
+                // expect(result['name']).to.equal('한강희')
+                // expect(result['sex']).to.equal('남')
                 done()
             })
     })
 })
+
+
+
+suite('GET /user/check', () => {
+    const getToken = new Promise(
+        (resolve, reject) => {
+            request(app.listen())
+                .post('/user/login')
+                .send({ userID: "test", pwd: "1234"})
+                .expect(200)
+                .end((err, res) => {
+                    if(err){
+                        reject(err)
+                    }
+                    let token = res.body.token
+                    resolve(token)
+                })
+        }
+    )
+    test('should respond with user Data', (done) => {
+        getToken.then(token => {
+            let url = `/user/check/?token=${token}`
+            console.log(url)
+            request(app.listen())
+                .get(url)
+                .expect(200)
+                .end((err, res) => {
+                    if(err){
+                        done(err)
+                        return
+                    }
+                    let result = res.body.user
+                    expect(result['userID']).to.equal('test')
+                    expect(result['pwd']).to.equal('1234')
+                    done()
+                })
+        })
+    })
+
+})
+//
+// suite('GET /user/join', () => {
+//     test('should respond with text message "ok"', (done) => {
+//         request(app.listen())
+//             .post('/user/join')
+//             .send({ userID: "test6", pwd: "1234", name: "조잔형", sex:"남"})
+//             .expect(200)
+//             .end((err, res) => {
+//                 if(err){
+//                     done(err)
+//                     return
+//                 }
+//                 let result = res.body
+//                 console.log(result)
+//                 expect(result["message"]).to.equal('ok')
+//                 done()
+//             })
+//     })
+// })
 
