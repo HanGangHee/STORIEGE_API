f6e94ca0d3be62fbdcc3c1fc3d276ccd741321af
HanGangHee <tkdlaos21@ajou.ac.kr>
2018-07-24 17:05:54 +0900

    route 정리

--- app.js
+++ app.js
@@ -32,31 +32,3 @@ if(!module.parent){
 }
 console.log("Application started. Listening on port:" + port)
 
-
-
-
-// app.get('/1', function (req, res) {
-//     db.query('SELECT * FROM user', (err, rows, fields) => {
-//         if(err) throw err
-//         let result
-//         console.log(JSON.stringify(rows))
-//         result = JSON.stringify(rows[0])
-//         res.send(result)
-//     })
-// })
-// app.get('/2', function (req, res) {
-//     res.type('text/plain')
-//     res.send("한강희's  /2  change")
-// })
-//
-// app.use(function (req, res) {
-//     res.status(404)
-//     res.render('404')
-// })
-//
-// app.use(function (err, req, res, next) {
-//     console.error(err.stack)
-//     res.status(500)
-//     res.render('500')
-// })
-
