3f7a735b2e9adfd03e7a9cdc43e8137ff912144d
HanGangHee <tkdlaos21@ajou.ac.kr>
2018-08-06 13:39:09 +0900

    wiki schema

---
+++ models/wikis.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const wikisSchema = new Schema (
+    {
+        no: [
+                {
+                    version: Number,
+                    title : String,
+                    content : String,
+                    write_time: Date,
+                    user_id: String,
+                    parent_no : Number,
+                    likes : Number,
+                    dislikes : Number,
+                    tags : Array
+                }
+        ]
+    }
+)
